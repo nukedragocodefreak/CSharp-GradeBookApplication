b85cbe24bb69fb2dee72c1a7379cd0afb62cc085
Noel Mangwarira <NoelM@betsoftware.net>
2022-01-17 08:21:04 +0200

    Create CalculateStatistics Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -53,7 +53,9 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
+            base.CalculateStatistics();
         }
     }
 }
